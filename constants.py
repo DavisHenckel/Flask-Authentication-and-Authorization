d1c68cd054de57bca522a21d4f7a532240f98317
DavisHenckel <davishenckel@hotmail.com>
2020-11-13 16:51:08 -0800

    initial setup for project complete

---
+++ constants.py
@@ -0,0 +1,3 @@
+boats = "boats"
+jsonErrorStringArgs = '{ "Error":"The request object is missing at least one of the required attributes"}'
+jsonOnlyAcceptJson = '{ "Error":"This method only accepts JSON"}'
